38d97d3d85184874d90d4491ee11f71dc197db42
Iegor <iegor@xuntos.nl>
2018-12-02 21:43:51 +0200

    Add validation for view model

--- DemoF.Web/ViewModels/UserViewModel.cs
+++ DemoF.Web/ViewModels/UserViewModel.cs
@@ -4,13 +4,16 @@ namespace DemoF.Web.ViewModels
 {
     public class UserViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "You must enter First name")]
+        [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z-_]*$", ErrorMessage = "Use Characters only")]
         [StringLength(30)]
         public string FirstName { get; set; }
 
+        [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z-_]*$", ErrorMessage = "Use Characters only")]
         public string MiddleName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "You must enter Last name")]
+        [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z-_]*$", ErrorMessage = "Use Characters only")]
         public string LastName { get; set; }
     }
 }
